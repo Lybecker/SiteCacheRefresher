adf985a55b20a5c331f4d45880dc8db235ad13cc
Anders Lybecker <anders@lybecker.com>
2018-12-11 11:57:13 +0100

    Download page with images, css etc.

--- SiteRender/index.js
+++ SiteRender/index.js
@@ -1,9 +1,31 @@
+const scrape = require('website-scraper');
+const rimraf = require('rimraf');
+
 module.exports = async function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
     
-    if(myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+    var siteUrls = process.env["SiteURLs"].split(";");
+    context.log('Download site(s) URLs ', siteUrls);
+
+    var dirPath = '/temp';
+
+    // website-scraper requires the output directory does not exist
+    rimraf.sync(dirPath);
+
+    const options = {
+        urls: siteUrls,
+        urlFilter: function(url) { return false },
+        directory: dirPath,
+        ignoreErrors: true,
+        request: {
+            headers: {
+              'User-Agent': 'Mozilla/5.0 (Azure Function) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19'
+            }
+        }
+    };
+    
+    var hrstart = process.hrtime();
+    const result = await scrape(options);
+    hrend = process.hrtime(hrstart);
+
+    context.log('Download site(s) time: %ds %dms', hrend[0], hrend[1] / 1000000)
 };
